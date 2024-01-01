116f887ef5ec492ab63f3354ca43424914e6dac2
jaynila077 <jaynila077@gmail.com>
2024-01-01 14:36:42 +0530

    Add store service exports and remove duplicate imports

---
+++ server/service/store/index.js
@@ -0,0 +1,6 @@
+import getStoreService from "../store/storeServices/get";
+import postStoreService from "../store/storeServices/post";
+import putStoreService from "../store/storeServices/put";
+import deleteStoreService from "../store/storeServices/delete";
+
+export { getStoreService, postStoreService, putStoreService, deleteStoreService };
--- server/service/user/index.js
+++ server/service/user/index.js
@@ -4,11 +4,5 @@ import putUserService from "./userServices/put";
 import deleteUserService from "./userServices/delete";
 import loginService from "./loginServices/loginServices";
 
-import getStoreService from "../store/storeServices/get";
-import postStoreService from "../store/storeServices/post";
-import putStoreService from "../store/storeServices/put";
-import deleteStoreService from "../store/storeServices/delete";
-
 
 export { getUserService, postUserService, putUserService, deleteUserService, loginService };
-export { getStoreService, postStoreService, putStoreService, deleteStoreService };
