2362c055a3ba2e4ec485ed65162ed5d385bd5b66
jaynila077 <jaynila077@gmail.com>
2024-01-01 13:38:29 +0530

    feat: jwt auth

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import userController  from "../../controllers/users/index.js";
 import loginController from "../../controllers/users/login/index.js";
+import authenticateToken from "../../middleware/auth.js";
 
 const User = express.Router();
 
-User.get("/", userController.getAllUser);
-User.get("/:id", userController.getUserById);
-User.post("/", userController.postUser);
-User.put("/:id", userController.putUser);
-User.delete("/:id", userController.removeUser);
+User.get("/", authenticateToken,userController.getAllUser);
+User.get("/:id",authenticateToken, userController.getUserById);
+User.post("/", authenticateToken,userController.postUser);
+User.put("/:id", authenticateToken,userController.putUser);
+User.delete("/:id", authenticateToken,userController.removeUser);
 
 User.post("/login", loginController.loginUser);
 
