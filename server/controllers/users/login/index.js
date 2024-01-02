ce2dc8a01e9c4392d2e2cd7947c49a68e4ed722d
jaynila077 <jaynila077@gmail.com>
2024-01-02 22:33:58 +0530

    update

--- server/controllers/users/login/index.js
+++ server/controllers/users/login/index.js
@@ -6,8 +6,6 @@ const loginUser = async (req, res) => {
 
     const user = await loginService.authenticateUser(name, password);
 
-    //JWT need to be added here.
-    
     res.status(200).json({ success: true, data: user });
   } catch (err) {
     console.error('Error in loginUser controller:', err);
